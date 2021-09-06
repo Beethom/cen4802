1b1d448365cb8c0b281046a8b9aacf529ad9f598
Beethom <bmarhone@mail.valenciacollege.edu>
2021-09-06 00:30:05 -0400

    I added javadoc

--- Fibonacci/src/App.java
+++ Fibonacci/src/App.java
@@ -1,5 +1,16 @@
+  /**
+* Create a  recursive static method that returns the nth term in the Fibonacci sequence. 
+*   The method should accept n as a parameter.
+*  main method that calls the first method.
+* @param  integer the number  to display.  If the number  is an intger
+* @param n Number to be Sequenced Recursively by Fibonacci Method
+ * @return Returns the result of number sequenced by Fibonacci Method
+* @author   Beethovrn Marhone         
+*/
 
 public class App {
+  
+// main method that calls the fib method and passing the designated number.
     public static void main(String[] args) {
 
         int nth = 10;
@@ -8,7 +19,7 @@ public class App {
         System.out.println("The " + nth + " term of the Fibonacci sequence is " + fib);
 
     }
-
+//  recursive method that will take any number , and return the fib term.
     private static int fibRe(int n) {
         if (n == 0)
             return 0;
